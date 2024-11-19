15a79ce2b944cf35649ff25d8f7db208f8bf252a
Gustavo <gugavaladao@gmail.com>
2024-11-18 21:48:04 -0300

    Adicionada a pratica 11

---
+++ praticas/pratica11/front/src/components/InputEmail.jsx
@@ -0,0 +1,20 @@
+function InputEmail(props) {
+
+    const regras = {
+        required: "Email é obrigatório",
+        pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: 'Formato de e-mail inválido',
+        }
+    }
+
+    return (
+        <>
+            <label htmlFor="email">Email: </label>
+            <input type="email" {...props.register("email", regras)}/>
+            {props.error && <p>{props.error.message}</p>}
+        </>
+    )
+}
+
+export default InputEmail;
